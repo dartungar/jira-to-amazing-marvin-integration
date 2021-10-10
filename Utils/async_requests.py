06ae7bfcf33238a078195c80e57732c5acb829bc
Danila Nikolayev <D.Nikolayev@Treolan.ru>
2021-10-10 18:17:02 +0300

    async web requests & better logging

---
+++ Utils/async_requests.py
@@ -0,0 +1,23 @@
+import aiohttp
+from Utils.logger import logger
+
+async def async_get(url: str, headers: dict = None, params: dict = None) -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers, params=params) as response:
+            data: dict = await response.json()
+            if not response.ok:
+                raise HttpRequestException(f"Error while making GET request to {url}: {response.status} {response.reason}")
+            return data
+
+async def async_post(url: str, headers: dict = None, data: str = None) -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, data=data) as response:
+            resp_data = await response.json()
+            if not response.ok:
+                raise HttpRequestException(f"Error while making POST request to {url}: {response.status}")
+            return resp_data
+
+
+
+class HttpRequestException(Exception):
+    pass
